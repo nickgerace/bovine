5fc9149d1e2f14e21d2588ff744bc4dfeecfced7
Nick Gerace <nickagerace@gmail.com>
2021-10-07 01:20:49 -0400

    Handle docker permissions error in version output

--- src/types/version.rs
+++ src/types/version.rs
@@ -34,10 +34,17 @@ pub struct DockerVersion {
     #[serde(skip_serializing_if = "Option::is_none")]
     pub docker_socket_path: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
+    pub raw_error: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub error: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub help: Option<String>,
 }
 
-pub fn new_docker_version_with_connection_failure() -> DockerVersion {
+pub fn new_docker_version_with_connection_failure(
+    bollard_error: Option<bollard::errors::Error>,
+    bovine_error: Option<Error>,
+) -> DockerVersion {
     DockerVersion {
         version: None,
         os_arch: None,
@@ -45,6 +52,8 @@ pub fn new_docker_version_with_connection_failure() -> DockerVersion {
         linux_kernel_version: None,
         git_commit: None,
         docker_socket_path: None,
-        error: Some(Error::DockerSocketConnectFailure.to_string()),
+        raw_error: bollard_error.map(|e| e.to_string()),
+        error: bovine_error.map(|e| e.to_string()),
+        help: Some(Error::DockerSocketConnectFailure.to_string()),
     }
 }
